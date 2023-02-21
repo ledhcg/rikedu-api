462df1540b8f5f8d4712812bd25d24d3c9218778
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-21 06:08:20 +0300

    Config table "posts"

---
+++ database/migrations/2023_02_20_035446_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->string('thumbnail')->nullable();
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->text('summary');
+            $table->boolean('published')->default(false);
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
