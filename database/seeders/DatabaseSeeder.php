022eb665bdc0f68dd37a911060ae1c8c9995fe8c
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-21 07:08:14 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::factory(10)->hasPosts(25)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
