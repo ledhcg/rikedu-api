bff0f9284824b198ce422b3580933f6277f72ab7
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-21 07:09:42 +0300

    Create PostCollection.php

---
+++ app/Http/Resources/PostCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'posts' => PostResource::collection($this->collection),
+            'meta' => [
+                'total' => $this->total(),
+                'per_page' => $this->perPage(),
+                'current_page' => $this->currentPage(),
+                'last_page' => $this->lastPage(),
+                'from' => $this->firstItem(),
+                'to' => $this->lastItem(),
+            ],
+        ];
+    }
+}
