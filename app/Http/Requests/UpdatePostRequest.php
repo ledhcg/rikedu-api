99a972648785f860c25f01e66c7a4fa3bf2bc71b
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-21 07:09:36 +0300

    Create UpdatePostRequest.php

---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'sometimes|required|string|max:255',
+            'content' => 'sometimes|required|string',
+            'thumbnail' => 'sometimes|required|url',
+            'published' => 'sometimes|required|boolean',
+        ];
+    }
+}
