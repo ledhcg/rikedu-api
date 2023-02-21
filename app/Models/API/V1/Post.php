4b9790d4e5bfc697abfec9467774d21fa9630327
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-21 06:07:37 +0300

    Config model Post

---
+++ app/Models/API/V1/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\API\V1;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasUuid;
+
+class Post extends Model
+{
+    use HasFactory, HasUuid;
+    
+    protected $fillable = [
+        'user_id',
+        'title',
+        'thumbnail',
+        'slug',
+        'content',
+        'summary',
+        'published',
+        'published_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+}
