de2de5c4916bffd73aa570cf5b68861423d7b29d
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-21 07:10:23 +0300

    change user_id use uuid

--- database/migrations/2023_02_20_035446_create_posts_table.php
+++ database/migrations/2023_02_20_035446_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignId('user_id')->constrained();
+            $table->uuid('user_id')->constrained();
             $table->string('title');
             $table->string('thumbnail')->nullable();
             $table->string('slug')->unique();
