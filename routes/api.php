d760a44544f41c8fb2e608d234860768a8416ff5
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-22 10:43:09 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\PostController;
+use App\Http\Controllers\API\V1\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +20,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/posts', [PostController::class, 'index']);
-// Route::get('/posts/{id}', [PostController::class, 'show']);
-// Route::post('/posts', [PostController::class, 'store']);
-// Route::put('/posts/{id}', [PostController::class, 'update']);
-// Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
 
-Route::group(['prefix'=>'v1', 'namespace' => 'App\Http\Controllers\API\V1'], function(){
-    Route::resource('posts', PostController::class);
+Route::group(['prefix'=>'v1', 'namespace' => 'App\Http\Controllers\API\V1', 'middleware' => ['auth:sanctum']], function(){
+    // Route::get('posts', PostController::class);
+    // Route::resource('posts', PostController::class);
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::get('/posts/{id}', [PostController::class, 'show']);
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+    
+    
+});
+Route::group(['prefix'=>'v1'], function(){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login'); 
 });
