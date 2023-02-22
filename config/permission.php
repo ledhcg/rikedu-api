e8aea15bed02aff190bebfa691bf8fab044fd378
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-22 10:40:52 +0300

    Update permission.php
    Chang path models, use uuid (model_id -> model_uuid)

--- config/permission.php
+++ config/permission.php
@@ -13,7 +13,9 @@ return [
          * `Spatie\Permission\Contracts\Permission` contract.
          */
 
-        'permission' => Spatie\Permission\Models\Permission::class,
+        // 'permission' => Spatie\Permission\Models\Permission::class,
+        'permission' => App\Models\API\V1\Permission::class,
+
 
         /*
          * When using the "HasRoles" trait from this package, we need to know which
@@ -24,7 +26,9 @@ return [
          * `Spatie\Permission\Contracts\Role` contract.
          */
 
-        'role' => Spatie\Permission\Models\Role::class,
+        // 'role' => Spatie\Permission\Models\Role::class,
+        'role' => App\Models\API\V1\Role::class,
+
 
     ],
 
@@ -86,7 +90,8 @@ return [
          * that case, name this `model_uuid`.
          */
 
-        'model_morph_key' => 'model_id',
+        // 'model_morph_key' => 'model_id',
+        'model_morph_key' => 'model_uuid',
 
         /*
          * Change this if you want to use the teams feature and your related model's
