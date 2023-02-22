59540f4b8f9dfe228876f25d5800d516f4a214fe
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-22 10:43:04 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\API\V1\Role;
+use App\Models\API\V1\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Create roles
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+
+        // Create permissions
+        $createPostsPermission = Permission::create(['name' => 'create posts']);
+
+        $updateOwnPostsPermission = Permission::create(['name' => 'update own posts']);
+        $deleteOwnPostsPermission = Permission::create(['name' => 'delete own posts']);
+        
+        $updateAnyPostPermission = Permission::create(['name' => 'update any post']);
+        $deleteAnyPostPermission = Permission::create(['name' => 'delete any post']);
+
+        // Assign permissions to roles
+        $adminRole->givePermissionTo($createPostsPermission);
+        $adminRole->givePermissionTo($updateAnyPostPermission);
+        $adminRole->givePermissionTo($deleteAnyPostPermission);
+
+        $userRole->givePermissionTo($updateOwnPostsPermission);
+        $userRole->givePermissionTo($deleteOwnPostsPermission);
+        
         \App\Models\User::factory(10)->hasPosts(25)->create();
         // \App\Models\User::factory(10)->create();
 
