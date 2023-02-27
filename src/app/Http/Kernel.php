69d28ef4196fc735b8d357787c893c46fd7a5245
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-02-28 01:39:01 +0300

    Fix CORS

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
+        // Add custom CORS
+        \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
