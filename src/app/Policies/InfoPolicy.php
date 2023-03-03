e4e491954fb660806fa5bc9d856a828155879d34
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-03 11:52:26 +0300

    Create InfoPolicy.php

---
+++ src/app/Policies/InfoPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Info;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class InfoPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Info  $info
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Info $info)
+    {
+        //
+    }
+}
