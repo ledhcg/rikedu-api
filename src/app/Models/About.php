b419688f1ca99914fa649ff5aaace9e50a331ec9
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-11 05:49:46 +0300

    Fix image

--- src/app/Models/About.php
+++ src/app/Models/About.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use App\Contracts\StoragePath;
 
 class About extends Model
 {
@@ -14,13 +16,30 @@ class About extends Model
     protected $fillable = [
         'user_id',
         'title',
-        'thumbnail',
+        'image',
         'slug',
         'content',
         'summary',
         'published_at',
     ];
 
+    public function getImageThumbnailUrlAttribute()
+    {
+        //Check if $image is a URL or not, and then return data accordingly
+        $image = $this->attributes['image'];
+        return filter_var($image, FILTER_VALIDATE_URL)
+            ? $image
+            : asset(Storage::url(StoragePath::ABOUT_IMAGE_THUMBNAIL . $image));
+    }
+
+    public function getImageCoverUrlAttribute()
+    {
+        //Check if $image is a URL or not, and then return data accordingly
+        $image = $this->attributes['image'];
+        return filter_var($image, FILTER_VALIDATE_URL)
+            ? $image
+            : asset(Storage::url(StoragePath::ABOUT_IMAGE_COVER . $image));
+    }
 
     public function user()
     {
