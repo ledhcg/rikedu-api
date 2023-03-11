be48eacda2a0f46ac2197f0efa8506527ea694dc
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-11 05:55:52 +0300

    Update 2023_03_01_113840_create_posts_table.php

--- src/database/migrations/2023_03_01_113840_create_posts_table.php
+++ src/database/migrations/2023_03_01_113840_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -17,9 +16,9 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->uuid('user_id')->constrained();
             $table->string('title');
-            $table->string('thumbnail')->nullable();
+            $table->string('image')->nullable();
             $table->string('slug')->unique();
-            $table->text('content');
+            $table->longText('content');
             $table->text('summary');
             $table->boolean('published')->default(false);
             $table->timestamp('published_at')->nullable();
