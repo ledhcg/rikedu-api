30f662b17aea2c164e41f988aa844fcde67d0345
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-11 05:38:46 +0300

    Update StoragePath.php

--- src/app/Contracts/StoragePath.php
+++ src/app/Contracts/StoragePath.php
@@ -4,8 +4,14 @@ namespace App\Contracts;
 
 interface StoragePath
 {
-    const INFO_THUMBNAIL = 'storage/images/info/thumbnail/';
-    const INFO_COVER = 'storage/images/info/cover/';
+    const INFO_IMAGE_COVER = 'images/info/cover/';
+    const INFO_IMAGE_THUMBNAIL = 'images/info/thumbnail/';
 
-    const USER_IMAGE = 'storage/images/user/avatar';
+    const ABOUT_IMAGE_COVER = 'images/about/cover/';
+    const ABOUT_IMAGE_THUMBNAIL = 'images/about/thumbnail/';
+
+    const USER_IMAGE_AVATAR = 'images/user/avatar/';
+
+    const POST_IMAGE_COVER = 'images/post/cover/';
+    const POST_IMAGE_THUMBNAIL = 'images/post/thumbnail/';
 }
