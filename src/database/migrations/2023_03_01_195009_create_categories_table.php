1c9f9f592091c24f568f1dd3ab95c558a51eb647
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-11 05:55:42 +0300

    Update 2023_03_01_195009_create_categories_table.php

--- src/database/migrations/2023_03_01_195009_create_categories_table.php
+++ src/database/migrations/2023_03_01_195009_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,25 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+
+        Schema::create('post_has_category', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table
+                ->foreignUuid('category_id')
+                ->references('id')
+                ->on('categories');
+            $table
+                ->foreignUuid('post_id')
+                ->references('id')
+                ->on('posts');
         });
     }
 
@@ -27,5 +43,6 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('categories');
+        Schema::dropIfExists('post_has_category');
     }
 };
