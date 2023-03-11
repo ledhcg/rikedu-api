0f0ab4cc35ec098317f32e149efffacba41583ce
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-03-11 05:50:19 +0300

    Add Category and Tag

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasUuid;
+use App\Models\Post;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuid;
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'post_has_category');
+    }
 }
