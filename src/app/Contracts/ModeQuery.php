87bfee0e2487f1cf7b86c68dad0fade33ca8f4c8
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-05-09 20:30:58 +0300

    Update ModeQuery.php

--- src/app/Contracts/ModeQuery.php
+++ src/app/Contracts/ModeQuery.php
@@ -7,6 +7,10 @@ interface ModeQuery
     const MODEL_SINGLE = 'single';
     const MODEL_COLLECTION = 'collection';
 
+    const MODEL_USER_PARENT = 'parent';
+    const MODEL_USER_TEACHER = 'teacher';
+    const MODEL_USER_STUDENT = 'student';
+
     const GROUP_BY_CATEGORY = 'category';
     const GROUP_BY_TAG = 'tag';
 }
