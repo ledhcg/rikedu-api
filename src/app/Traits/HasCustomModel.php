4890a8cc686de74d732ef1717def9ee7110ddb66
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-05-09 20:36:05 +0300

    Fix 🐛

--- src/app/Traits/HasCustomModel.php
+++ src/app/Traits/HasCustomModel.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Traits;
+
 use Illuminate\Support\Facades\Storage;
 
 trait HasCustomModel
@@ -10,14 +11,14 @@ trait HasCustomModel
         //Check if $image is a URL or not, and then return data accordingly
         $image = $this->attributes['image'];
         return filter_var($image, FILTER_VALIDATE_URL)
-            ? $image
-            : secure_asset(Storage::url($pathSource . $image));
+        ? $image
+        : secure_asset(Storage::url($pathSource . $image));
     }
 
     public function getFullNameAttribute()
     {
-        return $this->attributes['first_name'] .
-            ' ' .
-            $this->attributes['last_name'];
+        return
+        $this->attributes['last_name'] . ' ' . $this->attributes['first_name']
+        ;
     }
 }
