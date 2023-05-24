a598420c0ce7be6e72d127efefee21ef13a57393
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-05-24 20:43:04 +0300

    Add Exam

---
+++ src/app/Http/Controllers/ExamController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreExamRequest;
+use App\Http\Requests\UpdateExamRequest;
+use App\Http\Resources\ExamCollection;
+use App\Models\Exam;
+
+class ExamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function listByStudent($studentID)
+    {
+        $exams = Exam::where('student_id', $studentID)->get();
+        if (!$exams) {
+            return $this->notFoundResponse('Exams not found');
+        }
+        return $this->successResponse(
+            new ExamCollection($exams),
+            'Exams retrieved successfully'
+        );
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreExamRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreExamRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Exam  $exam
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Exam $exam)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Exam  $exam
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Exam $exam)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateExamRequest  $request
+     * @param  \App\Models\Exam  $exam
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateExamRequest $request, Exam $exam)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Exam  $exam
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Exam $exam)
+    {
+        //
+    }
+}
