d1730428c7362c0f4e7a60ec013278339ae8ae72
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-06-04 01:13:14 +0300

    Update 2023_03_01_113840_create_posts_table.php

--- src/database/migrations/2023_03_01_113840_create_posts_table.php
+++ src/database/migrations/2023_03_01_113840_create_posts_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -14,7 +15,7 @@ return new class extends Migration {
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->uuid('user_id')->constrained();
+            $table->foreignUuid('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('title');
             $table->string('image')->nullable();
             $table->string('slug')->unique();
