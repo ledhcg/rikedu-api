3c98b1b2710c80001e678360d8a8142511b3fd08
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2023-06-04 01:11:07 +0300

    Update domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.rikedu.tech)
+domains=(api.rikedu.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="info@rikedu.tech" # Adding a valid address is strongly recommended
+email="info@rikedu.ru" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
